c89d9c509b5c31bb3148c3a2a7be7c8994161637
PJ-568 <pj568@pj568.eu.org>
2025-02-13 18:46:24 +0800

    【测试】更新登陆迎宾和 sudo 权限

--- profiledef.sh
+++ profiledef.sh
@@ -22,6 +22,7 @@ file_permissions=(
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/root/.gnupg"]="0:0:700"
+  ["/etc/sudoers.d"]="0:0:750"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/dfiles"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
